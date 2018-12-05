b54e80bb6375ce22acd335eaa2d163eb72b75aa0
Mario Infante <marioinfantejr@gmail.com>
2018-12-04 23:32:50 -0800

    added toolbar and skipping login page for now

--- app/src/main/java/cs184/cs/ucsb/edu/couchsurfer/MainActivity.java
+++ app/src/main/java/cs184/cs/ucsb/edu/couchsurfer/MainActivity.java
@@ -1,15 +1,44 @@
 package cs184.cs.ucsb.edu.couchsurfer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
-    // Testing a push
-    // testing second push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attaching the layout to the toolbar object
+        toolbar = findViewById(R.id.tool_bar);
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
